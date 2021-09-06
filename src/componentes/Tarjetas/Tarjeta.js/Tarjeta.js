7d074516e6895d4044cd4a217bd90fa8dc355cb5
Federico Koch <fkoch@udesa.edu.ar>
2021-09-06 19:01:29 -0300

    ver mas listo

--- src/componentes/Tarjetas/Tarjeta.js/Tarjeta.js
+++ src/componentes/Tarjetas/Tarjeta.js/Tarjeta.js
@@ -1,15 +1,43 @@
-import React from 'react'
-function Tarjeta(props) {
-    let {title, overview, poster_path, popularity} = props.info //esto es un destructuring
-    return (
-      <div>
-          <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="" />
-         <h1>{title}</h1>
-         <p>{overview}</p>
-         <p>popularity: {popularity}</p>
+import React,{Component} from 'react' 
+import "./Tarjeta.css" 
+class Tarjeta extends Component {
+    constructor(){ //uno guarda los estados
+      super()
+      this.state={
+        viewMore: false, //cuando recien abris la pagina, va a estar en falso
+        text: "Ver Mas", //opcion de ver mas
+      }
+      
+    }
+    VerMas(){ //Si al clickearlo, this.state.viewmore es true te aparece ver mas. Si es falso, ver menos. 
+      if(this.state.viewMore){
+        this.setState({
+          viewMore:false,
+          text:"Ver Mas",
+        })
+      }else{      
+      this.setState({
+        viewMore:true,
+        text:"Ver Menos", 
+      })}
+    }
+
+    render(){ //modulo que permite que las cosas se exporte, trabaja en JSX, pero por fuera en JS
+      let {title, overview, poster_path, popularity} = this.props.info
+      return ( //se trabaja en JSX
+        <div>
+            <img className= "image" src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="" />
+           <h1>{title}</h1>
+           <ul className= {`${this.state.viewMore ? "show" : "hide"}`}>  {/*if ternario, si es falso, se oculta y si es true te da show*/}
+           <p>{overview}</p>
+           <p>popularity: {popularity}</p></ul>
+      <p className = "ViewMore" onClick = {() => this.VerMas()}>{this.state.text}</p> {/*esta funcion cambia el estado. Ver mas es un modulo.*/}
+      
+        </div> 
+      );
+    }
+   //esto es un destructuring
     
-      </div> 
-    );
   }
 
   export default Tarjeta; 
