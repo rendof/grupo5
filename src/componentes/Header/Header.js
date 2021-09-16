a6bcdaf41fe48fb5c6b572d6a0fd767e661dc335
Federico Koch <fkoch@udesa.edu.ar>
2021-09-15 23:47:07 -0300

    Titulo cambiado

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -6,9 +6,9 @@ function Header(props) { //Componente sin estado
     return (
       <div className='HeaderGral'>
         
-        <h1 className='titulo'>ChillingTime </h1>
+        <h1 className='titulo'><a href="/">ChillingTime</a> </h1>
         <section className='SubHeader'>
-            <button>Ordenar ASC/ DESC</button>
+            
             <i className="orientacion" onClick = {()=>props.Vertical()} >Vertical</i> 
             <i className="orientacion"onClick = {()=>props.Horizontal()}>Horizontal</i>
             <Buscador Buscar = { (texto)=> props.Buscar(texto) }/>
